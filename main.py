d93ca8f77480a9fc6944794149ca046872dbf60f
NIDHI <89473516+Nidhi-Pawar@users.noreply.github.com>
2025-03-24 23:25:57 +0530

    Create and update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(message)s')
+
+logging.info("This is an info message")
+logging.debug("This is a debug message")
+logging.warning("This is a warning")
