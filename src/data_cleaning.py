30f3ae6179fbf431a79652150567911ade4f12ed
NIDHI <89473516+Nidhi-Pawar@users.noreply.github.com>
2025-03-26 02:33:39 +0530

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,8 +2,8 @@ import logging
 from abc import ABC, abstractmethod
 from typing import Union, Tuple
 
-import numpy as np
 import pandas as pd
+from sklearn.preprocessing import PowerTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
@@ -22,91 +22,119 @@ class DataPreprocessStrategy(DataStrategy):
     Abstract Class defining strategies for handling data preprocessing
     
     """
-    def handle_data(self, data) -> pd.DataFrame:
-        try:
-            d = {'10+ years':10, '9 years':9, '8 years':8, '7 years':7, '6 years':6,
-                '5 years':5, '4 years':4, '3 years':3, '2 years':2,  '1 year':1,
-                '< 1 year':0 }
-            data['emp_length']=data['emp_length'].replace(d)
-            d = {' 36 months':36, ' 60 months':60}
-            data['term']=data['term'].replace(d)
+    def __init__(self, config):
+        self.config = config
 
-            d = {'Fully Paid':1, 'Charged Off':0}
-            data['loan_status']=data['loan_status'].replace(d)
+    def handle_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        data = data.copy()
+        data = self.convert_column_values(data)
+        data = self.handle_missing_values(data)
+        data = self.handle_outliers(data)
+        data = self.drop_columns(data)
+        data = self.frequency_encoding(data)
 
-            mort_avg = data.groupby('total_acc')['mort_acc'].mean()
-            def mort(total_acc, mort_acc):
-                if np.isnan(mort_acc):
-                    return mort_avg[total_acc].round()
-                else:
-                    return mort_acc
-            data['mort_acc'] = data.apply(lambda x: mort(x['total_acc'],x['mort_acc']), axis=1)
-            data.dropna(inplace=True)
-
-            def iqr_limits(series, multiplier=1.5):
-                Q1 = series.quantile(0.25)
-                Q3 = series.quantile(0.75)
-                IQR = Q3 - Q1
-                lower_limit = Q1 - multiplier * IQR
-                upper_limit = Q3 + multiplier * IQR
-                return lower_limit, upper_limit
-            
-            # 1. IQR method
-            cols = ['loan_amnt', 'int_rate', 'installment']
-            mask = pd.Series(True, index=data.index)
-            # Calculate the upper and lower limits
-            for col in cols:
-                ll, ul = iqr_limits(data[col], multiplier=1.5)
-                
-                # Update mask: Mark False where outliers exist in any column
-                mask &= (data[col] >= ll) & (data[col] <= ul)
-                outliers = data[(data[col] < ll) | (data[col] > ul)]
-                
-            # Apply the mask to filter out rows with outliers
-            data = data[mask]
-
-            #2. Quantile capping
-            cols = ['open_acc', 'revol_util', 'total_acc']
-
-            for col in cols:
-                lower = data[col].quantile(0.01)  # 1st percentile
-                upper = data[col].quantile(0.99)  # 99th percentile
-                
-                data = data[(data[col] >= lower) & (data[col] <= upper)]
-                outliers = data[(data[col] < ll) | (data[col] > ul)]
-    
-            #3. Yeo-Johnson transformation
-            from sklearn.preprocessing import PowerTransformer
-            cols = ['annual_inc', 'revol_bal', 'dti']
-            pt = PowerTransformer(method='yeo-johnson')
-
-            for col in cols:
-                data[col] = pt.fit_transform(data[[col]])
-                
-            #Dropping columns
-
-            data.drop(columns=['initial_list_status', 'emp_title', 'title','earliest_cr_line', 
-                            'issue_d', 'grade', 'sub_grade', 'installment','pub_rec_bankruptcies', 
-                            'application_type', 'address'], inplace=True)
-
-            # Get the indices of rows to drop
-            index_to_drop = data[data['home_ownership'].isin(['ANY', 'NONE'])].index
-            data = data.drop(index_to_drop)
-            
-
-            # Frequency encoding
-            cols = ['purpose', 'home_ownership', 'verification_status']
-            for col in cols:
-                freq_encoding = data[col].value_counts().to_dict()
-                data[col] = data[col].map(freq_encoding)
-              
-        except Exception as e:
-            logging.error('Error in data preprocessing')
-            raise e 
-        
         return data
-        
 
+    def convert_column_values(self, data):
+            """
+            Convert column values to appropriate data types
+
+            """
+            try:
+                if "emp_length" in data.columns:
+                    data['emp_length'] = data['emp_length'].str.extract('(\d+)').astype(int)
+                if "term" in data.columns:
+                    data['term'] = data['term'].str.extract('(\d+)').astype(int)
+                if "loan_status" in data.columns:
+                    data['loan_status'] = data['loan_status'].map({'Fully Paid': 1, 'Charged Off': 0})
+                    return data
+            except Exception as e:
+                logging.error('Error in converting column values')
+                raise e
+        
+    def handle_missing_values(self, data):
+            """
+            Handle missing values in the data
+
+            """
+            try:
+                if "mort_acc" in data.columns and data["mort_acc"].isnull().sum() > 0:
+                    median = data["mort_acc"].median()
+                    data["mort_acc"].fillna(median, inplace=True)
+
+                data.dropna(inplace=True)
+                return data
+            
+            except Exception as e:
+                logging.error('Error in handling missing values')
+                raise e
+            
+    def handle_outliers(self, data):
+            """
+            Handle outliers in the data
+            """
+            try:
+                # Handling outliers using IQR method
+                cols = self.config["columns"].get("iqr_cols",[])
+                mask = pd.Series(True, index=data.index)
+
+                for col in cols:
+                    IQR = data[col].quantile(0.75) - data[col].quantile(0.25)
+                    ll = data[col].quantile(0.25) - 1.5 * IQR
+                    ul = data[col].quantile(0.75) + 1.5 * IQR
+                    mask &= (data[col] >= ll) & (data[col] <= ul)
+                data = data[mask]
+
+                # Handling outliers using quantile method
+                cols = self.config["columns"].get("quantile_cols",[])
+                for col in cols:
+                    lower = data[col].quantile(0.01)  # 1st percentile
+                    upper = data[col].quantile(0.99)  # 99th percentile
+                    data = data[(data[col] >= lower) & (data[col] <= upper)]
+                
+                # Handling outliers using PowerTransformer
+                cols = self.config["columns"].get("pt_cols",[])
+                pt = PowerTransformer(method='yeo-johnson')
+                for col in cols:
+                    data[col] = pt.fit_transform(data[[col]])
+
+                return data
+            
+            except Exception as e:
+                logging.error('Error in handling outliers')
+                raise e
+            
+    def drop_columns(self, data):
+            """
+            Dropping unnecessary columns from the data
+            """
+            try:
+                data.drop(columns=self.config["columns"].get("drop_cols",[]), inplace=True)
+                
+                index_to_drop = data[data['home_ownership'].isin(['ANY', 'NONE'])].index
+                data = data.drop(index_to_drop)
+            
+                return data
+            
+            except Exception as e:
+                logging.error('Error in dropping columns')
+                raise e
+            
+    def frequency_encoding(self, data):
+            """
+            Frequency encoding for categorical columns
+            """
+            try:
+                cols = self.config["columns"].get("freq_cols",[])
+                for col in cols:
+                    freq_encoding = data[col].value_counts().to_dict()
+                    data[col] = data[col].map(freq_encoding)
+                return data
+            
+            except Exception as e:
+                logging.error('Error in frequency encoding')
+                raise e          
+        
 class DataDivideStrategy(DataStrategy):
     """
     Strategy for dividing data into training and testing sets.
