4eaf276168e338dffae6c40d86b7f1f374947fe6
NIDHI <89473516+Nidhi-Pawar@users.noreply.github.com>
2025-04-17 00:54:59 +0530

    Update training_pipeline.py

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,13 +1,16 @@
-from zenml import pipeline
+from zenml.pipelines import pipeline
 from steps.ingest_data import ingest_data
 from steps.clean_data import clean_data
 from steps.train_model import train_model
-from steps.evaluation import evaluate_model
+from steps.evaluation import evaluation
+from steps.config import ModelNameConfig
 
-@pipeline
+
+@pipeline(enable_cache= True)
 def train_pipeline(data_path: str):
     df = ingest_data(data_path)
-    clean_data(df)
-    train_model(df)
-    evaluate_model(df)
-    pass
+    X_train, y_train, X_test, y_test = clean_data(df)
+    config = ModelNameConfig() 
+    model = train_model(X_train, y_train, X_test, y_test, config)
+    evaluation(model, X_test, y_test)
+
