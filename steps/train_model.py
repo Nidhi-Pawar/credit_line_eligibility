572d495face4d7abac433f7d7ed5bbbf1b82ed44
NIDHI <89473516+Nidhi-Pawar@users.noreply.github.com>
2025-04-18 02:46:55 +0530

    Update train_model.py

--- steps/train_model.py
+++ steps/train_model.py
@@ -24,8 +24,6 @@ if experiment_tracker is None:
 
 def train_model(X_train: pd.DataFrame,
                 y_train: pd.Series,
-                X_test: pd.DataFrame,
-                y_test: pd.Series,
                 config: ModelNameConfig) -> ClassifierMixin:
     
     """
@@ -38,10 +36,8 @@ def train_model(X_train: pd.DataFrame,
     y_test (pd.Series): The testing labels.
     config (ModelNameConfig): The model configuration.
     """
-    if mlflow.active_run() is not None:
-            mlflow.end_run()
 
-    logging.info(f"Shapes of training data: X_train: {X_train.shape}, y_train: {y_train.shape}, X_test: {X_test.shape}, y_test: {y_test.shape}")
+    logging.info(f"Shapes of training data: X_train: {X_train.shape}, y_train: {y_train.shape}") 
     
     try:
         model = None
@@ -56,23 +52,21 @@ def train_model(X_train: pd.DataFrame,
         else:
             raise ValueError(f"Model {config.model_name} not supported")
 
-        tuner = HyperParameterTuner(model, X_train, y_train, X_test, y_test)
+        tuner = HyperParameterTuner(model, X_train, y_train)
 
         if config.fine_tuning:
-            best_params = tuner.optimize()
-            with mlflow.start_run(run_name=config.model_name, nested=True):
-                mlflow.log_params(best_params)
+            best_params = tuner.optimize(n_trials=30)
+            mlflow.log_params(best_params)
 
             trained_model = model.train(X_train, y_train, **best_params)   
             mlflow.sklearn.log_model(trained_model, artifact_path="model")
-            mlflow.end_run()
+            
             
         else:
             trained_model = model.train(X_train, y_train)
 
-            with mlflow.start_run(run_name=config.model_name):
-                mlflow.sklearn.log_model(trained_model, artifact_path="model")
-            mlflow.end_run()
+            mlflow.sklearn.log_model(trained_model, artifact_path="model")
+            
 
         return trained_model
             
